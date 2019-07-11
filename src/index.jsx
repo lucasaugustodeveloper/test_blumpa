c5797b62b6091071cba04e524af832f43f6d3498
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-11 18:07:40 -0300

    Change path of file App

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App/App'
+import App from './screens/App'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
 
